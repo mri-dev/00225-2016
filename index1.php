8ac0dd63177c3f378c138a206368b0c4b8db67af
mri-dev <pocokxtramepocokxtrame@gmail.com>
2016-10-05 13:26:42 +0200

    Save sw pc

--- index1.php
+++ index1.php
@@ -3,11 +3,11 @@
 <!-- Contents /s -->
 <div class="homepage base-content">
 	<div class="domain-searcher">
-		<form method="post" action="<?=$base?>/spincheck.php" id="check" name="check" onsubmit="showObject(false,true);">
-			<input type="text" style="padding:4px;" size="40" maxlength="272" name="cSSld" class="width-65" id="cSSld" placeholder="Írja be a választott domain nevét"/>
-			<input list="end" style="padding:4px;" name="endtag" size="4" placeholder=".HU"/>
-			<input type="submit" class="keresgomb" name="Submit" value="KERESÉS" />
-			<datalist id="end">
+		<form method="post" action="<?=$base?>/spincheck.php" id="check" name="check" onsubmit="showObject(false,true);"><!--
+	 --><div class="form-wrapper search-input"><input type="text" maxlength="272" name="cSSld" id="cSSld" placeholder="Írja be a választott domain nevét"/></div><!--
+	 --><div class="form-wrapper search-list"><input list="end" name="endtag" size="4" placeholder=".HU"/></div><!--
+	 --><div class="form-wrapper search-button"><button type="submit" class="keresgomb" name="Submit"/><i class="fa fa-search"></i> KERESÉS</div>
+	 		<datalist id="end">
 					<option value=".hu">
 					<option value=".com">
 			</datalist>
